1eaa0c58313cb546d0ae83d4f51915140be78071
Christopher Lightfoot <chris.lightfoot@methods.co.uk>
2020-12-17 09:40:50 +0000

    Day 17: What's another dimensions eh?...

--- 2020/day17.py
+++ 2020/day17.py
@@ -1,8 +1,5 @@
 # Day 14
 
-import re
-
-
 EMPTY = 0
 OCCUPIED = 1
 
@@ -39,7 +36,6 @@ def debug(cube):
             print()
 
 
-
 def part1(file):
     lines = [line.rstrip('\n') for line in open(file)]
 
@@ -48,36 +44,29 @@ def part1(file):
     for y in range(len(lines)):
         for x in range(len(lines[y])):
             if lines[y][x] == '#':
-                cube[(x,y,0)] = OCCUPIED
+                cube[(x, y, 0)] = OCCUPIED
 
     next = {}
 
     for i in range(6):
 
-        print("Cycle", i)
+        #print("Cycle", i)
 
         inactive_to_check = []
 
-        debug(cube)
+        #debug(cube)
 
         for c in cube:
-
-            #print(c, cube)
 
             if cube[c] == OCCUPIED:
 
                 count = 0
-
-                #print("check", c)
-
                 for dz in range(-1, 2):
                     for dy in range(-1, 2):
                         for dx in range(-1, 2):
                             # Skip center
                             if dx == 0 and dy == 0 and dz == 0:
                                 continue
-
-                            #print(" - nc", dx, dy, dz)
 
                             cx = dx + c[0]
                             cy = dy + c[1]
@@ -89,20 +78,13 @@ def part1(file):
                             if cube.get((cx, cy, cz), 0) == OCCUPIED:
                                 count += 1
 
-                #print("n count", c, count)
-
                 if count == 2 or count == 3:
                     # stay active; i.e. copy
                     next[c] = OCCUPIED
 
-        print("Done main loop; now about to check inactives that we came accross")
-        debug(next)
-        print(inactive_to_check)
-
         for c in inactive_to_check:
 
             count = 0
-
             for dz in range(-1, 2):
                 for dy in range(-1, 2):
                     for dx in range(-1, 2):
@@ -118,16 +100,84 @@ def part1(file):
                             count += 1
 
             if count == 3:
+                next[c] = OCCUPIED
+
+        cube = next
+        next = {}
+
+    return sum(cube.values())
+
+
+def part2(file):
+    lines = [line.rstrip('\n') for line in open(file)]
+
+    cube = {}
+
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            if lines[y][x] == '#':
+                cube[(x,y,0,0)] = OCCUPIED
+
+    next = {}
+
+    for i in range(6):
+
+        inactive_to_check = []
+
+        for c in cube:
+            if cube[c] == OCCUPIED:
+
+                count = 0
+                for dw in range(-1, 2):
+                    for dz in range(-1, 2):
+                        for dy in range(-1, 2):
+                            for dx in range(-1, 2):
+                                # Skip center
+                                if dx == 0 and dy == 0 and dz == 0 and dw == 0:
+                                    continue
+
+                                cx = dx + c[0]
+                                cy = dy + c[1]
+                                cz = dz + c[2]
+                                cw = dw + c[3]
+
+                                if cube.get((cx, cy, cz, cw), 0) == EMPTY:
+                                    inactive_to_check.append((cx, cy, cz, cw))
+
+                                if cube.get((cx, cy, cz, cw), 0) == OCCUPIED:
+                                    count += 1
+
+                if count == 2 or count == 3:
+                    # stay active; i.e. copy
+                    next[c] = OCCUPIED
+
+        for c in inactive_to_check:
+
+            count = 0
+            for dw in range(-1, 2):
+                for dz in range(-1, 2):
+                    for dy in range(-1, 2):
+                        for dx in range(-1, 2):
+                            # Skip center
+                            if dx == 0 and dy == 0 and dz == 0 and dw == 0:
+                                continue
+
+                            cx = dx + c[0]
+                            cy = dy + c[1]
+                            cz = dz + c[2]
+                            cw = dw + c[3]
+
+                            if cube.get((cx, cy, cz, cw), 0) == OCCUPIED:
+                                count += 1
+
+            if count == 3:
                 # become active
                 next[c] = 1
 
         cube = next
         next = {}
 
-
     return sum(cube.values())
-
-    #print(cube)
 
 # Run
 
@@ -135,6 +185,6 @@ print("# Part 1")
 print("- Test:", part1("day17/example.txt"), 112)
 print("- Input:", part1("day17/input.txt"), 5902420735773)
 
-# print("# Part 2")
-# print("- Example:", part2("day14/example2.txt"), 208)
-# print("- Input:", part2("day14/input.txt"), 3801988250775)
+print("# Part 2")
+print("- Example:", part2("day17/example.txt"), 848)
+print("- Input:", part2("day17/input.txt"), 1624)
