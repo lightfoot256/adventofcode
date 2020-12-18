c2a8632e5f00607a467b13cbf559d4c31d5439d5
Christopher Lightfoot <chris.lightfoot@methods.co.uk>
2020-12-18 00:25:34 +0000

    Day 15: Cleanup since part 2 didn't turn into what I thought it would.

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,6 +1,6 @@
 # Day 15
 
-def part1(str):
+def part1(str, turns):
     numbers = [int(n) for n in str.split(',')]
 
     history = {}
@@ -12,9 +12,7 @@ def part1(str):
         history[numbers[i]].append(i+1)
         spoken.append(numbers[i])
 
-    max = 2020
-
-    for turn in range(len(numbers)+1, max+1):
+    for turn in range(len(numbers)+1, turns+1):
 
         last = spoken[-1]
 
@@ -31,45 +29,15 @@ def part1(str):
 
     return spoken[-1]
 
-
-def part2(str):
-    numbers = [int(n) for n in str.split(',')]
-
-    history = {}
-    spoken = []
-
-    for i in range(0, len(numbers)):
-        if history.get(numbers[i],None) is None:
-            history[numbers[i]] = []
-        history[numbers[i]].append(i+1)
-        spoken.append(numbers[i])
-
-    max = 30000000
-
-    for turn in range(len(numbers)+1, max+1):
-        last = spoken[-1]
-
-        if len(history[last]) == 1:
-            speak = 0
-        else:
-            speak = history[last][-1] - history[last][-2]
-
-        if history.get(speak, None) is None:
-            history[speak] = []
-
-        history[speak].append(turn)
-        spoken.append(speak)
-
-    return spoken[-1]
-
 # Run
 
+
 print("# Part 1")
-print(" - Example", part1("0,3,6"), 436)
-print(" - Example", part1("1,3,2"), 1)
-print(" - Example", part1("2,1,3"), 10)
-print(" - Part1", part1("5,2,8,16,18,0,1"), 517)
+print(" - Example", part1("0,3,6", 2020), 436)
+print(" - Example", part1("1,3,2", 2020), 1)
+print(" - Example", part1("2,1,3", 2020), 10)
+print(" - Part1", part1("5,2,8,16,18,0,1", 2020), 517)
 
 print("# Part 2")
-print(" - Example", part2("0,3,6"), 175594)
-print(" - Part2", part2("5,2,8,16,18,0,1"), 175594)
+print(" - Example", part1("0,3,6", 30000000), 175594)
+print(" - Part2", part1("5,2,8,16,18,0,1", 30000000), 1047739)
